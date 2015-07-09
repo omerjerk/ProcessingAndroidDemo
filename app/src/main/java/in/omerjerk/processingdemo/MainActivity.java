a6bc9d802b2ee32212968a7a69e72f75ff70fcf5
Umair Khan <omerjerk@gmail.com>
2015-07-09 20:09:48 +0530

    update core
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/src/main/java/in/omerjerk/processingdemo/MainActivity.java
+++ app/src/main/java/in/omerjerk/processingdemo/MainActivity.java
@@ -4,8 +4,8 @@ import android.content.res.Configuration;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
@@ -74,7 +74,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onNavigationDrawerItemSelected(int position) {
         // update the main content by replacing fragments
-        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentManager fragmentManager = getFragmentManager();
         Fragment fragment;
         switch (position) {
             case 0:
